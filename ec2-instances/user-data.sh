32b45b81f5211557212e2b7f0e9c6bbbbb7b7efe
JustinKQuijanoo <quijanju@sheridancollege.ca>
2023-04-14 00:30:18 -0400

    switched from nginx to apache webserver

--- ec2-instances/user-data.sh
+++ ec2-instances/user-data.sh
@@ -5,13 +5,13 @@
 # Lab 3 - Terraform
 ###
 
-# install nginx webserver  
-sudo yum install nginx -y
+# install apache webserver  
+sudo yum install httpd -y
 
 # create index.html file 
 cd /var/www/html
 echo "<html><body><h1>Hello from Justin Quijano at $(hostname -f)</h1></body></html>" > index.html
 
-# restart and enable nginx service
-systemctl restart nginx
-systemctl enable nginx
+# restart and enable httpd service
+systemctl restart httpd
+systemctl enable httpd
