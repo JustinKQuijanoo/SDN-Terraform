1311e0b0c7e30b941a713f3c3f4aa038b4767a80
JustinKQuijanoo <quijanju@sheridancollege.ca>
2023-04-14 21:30:58 -0400

    added output for subnet_ids

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -4,12 +4,8 @@
 # Lab 3 - Terraform
 ###
 
-# Output for public and private subnet IDs to be used for ec2-instances module in the root main.tf
-output "public_subnet_id" {
-  value = aws_subnet.SN-Pub-Hydrohomie.id
-}
-output "private_subnet_id" {
-  value = aws_subnet.SN-Priv-Hydrohomie.id
+output "subnet_ids" {
+  value = aws_subnet.SN-Pub-Hydrohomie[*].id
 }
 
 # Output for VPC ID to be used for security-group module in the root main.tf
