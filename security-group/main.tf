ef6d70fb5dbf01ba110148ea0790e8270811cfb7
JustinKQuijanoo <quijanju@sheridancollege.ca>
2023-04-14 20:31:21 -0400

    added ingress entry for security groups

--- security-group/main.tf
+++ security-group/main.tf
@@ -24,6 +24,14 @@ resource "aws_security_group" "SG-Hydrohomie" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description     = "Allow HTTP from Security group"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "TCP"
+    security_groups = [aws_security_group.SG-Hydrohomie.id]
+  }
+
   egress {
     description = "Allow everything outbound"
     from_port   = 0
